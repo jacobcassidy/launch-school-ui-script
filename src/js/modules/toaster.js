8f8602358397234c21ad21fc1d5bc8fb1e2c6ade
Jacob Cassidy <jacob@jacobcassidy.com>
2026-07-14 14:41:51 +0700

    Refactor toaster function

--- src/js/modules/toaster.js
+++ src/js/modules/toaster.js
@@ -1,10 +1,16 @@
 /**
  * TOASTER
  */
-export function createToaster() {
-  console.log("Running createToaster()");
+import { colorLog } from "./helpers/log";
 
-  const toasterEl = document.createElement("div");
-  toasterEl.classList.add("toast-container");
-  document.body.appendChild(toasterEl);
+export function injectToaster() {
+  colorLog.run("Running injectToaster()");
+
+  const createToasterEl = () => {
+    const toasterEl = document.createElement("div");
+    toasterEl.classList.add("toast-container");
+    return toasterEl;
+  };
+
+  document.body.appendChild(createToasterEl());
 }
