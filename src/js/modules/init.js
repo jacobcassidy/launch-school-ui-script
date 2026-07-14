3c4e4132a71ca7a0a540e2011780c45410fe3711
Jacob Cassidy <jacob@jacobcassidy.com>
2026-07-14 11:10:58 +0700

    Update imported watch function name

--- src/js/modules/init.js
+++ src/js/modules/init.js
@@ -6,10 +6,10 @@
 
 import { colorLog } from "./helpers/utility.js";
 import { loadUI } from "./load.js";
-import { detectNavChange } from "./helpers/detection.js";
+import { watchForPageChange } from "./helpers/watch.js";
 
 export function initScript() {
   colorLog.run("Running initScript()");
   loadUI();
-  detectNavChange();
+  watchForPageChange();
 }
