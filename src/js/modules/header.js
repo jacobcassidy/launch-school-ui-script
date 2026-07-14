44b0b526ea0487c982fd438ec5eeb72cc2829e75
Jacob Cassidy <jacob@jacobcassidy.com>
2026-07-14 11:10:37 +0700

    Refactor header

--- src/js/modules/header.js
+++ src/js/modules/header.js
@@ -1,135 +1,172 @@
-/**
- * SITE HEADER FUNCTIONS
- */
 import { colorLog } from "./helpers/utility.js";
-import {
-  elements,
-  states,
-  getHeaderElement,
-  setHeaderElement,
-  setIsHeaderTop,
-  setIsHeaderPinned,
-  setIsHeaderUnpinned,
-} from "./helpers/state.js";
-import { hideSidebar, hideHeader, hideTabsPanel } from "./helpers/hide.js";
-import { showHeader, showSidebar, showTabsPanel } from "./helpers/show.js";
+import { getHeaderElement, getTabsPanelElement, setHeaderElement } from "./helpers/state.js";
+import { watchShowSidebarBtn } from "./helpers/watch.js";
 
-// import { elements, setIsHeaderPinned, setIsHeaderUnpinned } from "./state.js";
-
-const { header, tabsPanel, contentPanel, tabsPanelToggleButton } = elements;
-const { isHeaderPinned, isHeaderUnpinned, isHeaderTop } = states;
-
-// TODO - Update all siteHeader mods to include the mods in setSiteHeaderElement()
 /**
- * ADD SITE HEADER
+ * INJECT SITE HEADER
  */
-export function addHeader() {
-  console.log("Running addHeader()");
-
+export function injectHeader() {
+  colorLog.run("Running injectHeader()");
   const currentDomHeader = document.querySelector(".site-header");
 
-  if (getHeaderElement() !== currentDomHeader) setHeaderElement(currentDomHeader);
-  if (currentDomHeader) return;
+  if (!currentDomHeader) {
+    // Create header HTMLElement
+    const newHeader = createHeader();
 
-  // Native app nav items
-  const breadcrumbsEl = document.querySelector(".gretel-breadcrumbs");
-  const bookTocBtn = document.querySelector(".toc-toggle-button");
+    // Append the header HTMLElement into the DOM
+    document.body.insertBefore(newHeader, document.body.firstChild);
 
-  // Hide the sidebar if it's showing
-  hideSidebar();
+    // Inject offset styles into the DOM for .site-header__container
+    injectContainerStyleOffsets();
 
-  createHeader();
+    // Set header element properties after header is appended.
+    const header = document.querySelector(".site-header");
+    setHeaderElement(header);
 
-  const headerContainers = header.querySelectorAll(".site-header__container");
-  const headerContainer1 = headerContainers[0];
-  const headerContainer2 = headerContainers[1];
-  const headerContainer3 = headerContainers[2];
+    // const sidebarButton = document.querySelector(".btn--show-sidebar");
+    // setSidebarButtonElement(sidebarButton);
 
-  // Add elements to .site-header > .container-1
-  if (headerContainer1) {
-    createShowSidebarButton(headerContainer1);
-    moveLoggedOutNavToHeader(headerContainer1);
+    // setTabsPanelToggleButtonElement(document.querySelector(".btn--toggle-tabs-panel"));
+
+    // Watch header elements after the header is injected.
+    watchShowSidebarBtn();
+  } else {
+    if (currentDomHeader !== getHeaderElement()) setHeaderElement(currentDomHeader);
+  }
+}
+
+/**
+ * ADD CONTAINER ELEMENTS
+ * Adds elements to each .site-header__container
+ *
+ * @param {HTMLDivElement} containerEl The container to which the elements will be appended..
+ * @param {number} containerNum The number of the container to which the elements will be appended.
+ */
+function addContainerElements(containerEl, containerNum) {
+  if (containerNum === 1) {
+    createShowSidebarButton(containerEl);
+    addLoggedOutNavToHeader(containerEl);
   }
 
-  // Add elements to .site-header > .container-2
-  if (headerContainer2) {
-    // If breadcrumbs exist, move them inside the .site-header, otherwise add the title there.
-    if (breadcrumbsEl) {
-      headerContainer2.append(breadcrumbsEl);
+  if (containerNum === 2) {
+    // If breadcrumbs exist, move them inside the container, otherwise add the title there.
+    const breadcrumbs = document.querySelector(".gretel-breadcrumbs");
+    if (breadcrumbs) {
+      containerEl.append(breadcrumbs);
     } else {
-      addTitleToHeaderWithNoBreadcrumbs(headerContainer2);
+      addTitleToHeaderWithNoBreadcrumbs(containerEl);
     }
   }
 
-  // Add elements to .site-header > .container-3
-  if (headerContainer3) {
-    // Move book TOC button inside .site-header
+  if (containerNum === 3) {
+    // If a book TOC button exists, move it inside the container.
+    const bookTocBtn = document.querySelector(".toc-toggle-button");
     if (bookTocBtn) {
       bookTocBtn.classList.add("site-header__button");
-      headerContainer3.append(bookTocBtn);
+      containerEl.append(bookTocBtn);
     }
 
-    createToggleTabsPanelButton(headerContainer3);
-  }
-
-  // Set headerContainer CSS Custom Properties after all headerContainers modified
-  addHeaderContainerOffsets();
-
-  // TODO - Update this
-  // if (isHeaderHidden) {
-  //   header.classList.add("is-unpinned");
-  //   isHeaderPinned = false;
-  //   isHeaderUnpinned = true;
-  //   hideHeader();
-  // }
-}
-
-/**
- * UPDATE SITE HEADER VISIBILITY
- */
-// TODO - Update this
-export function updateHeaderVisibility() {
-  console.log("Running updateHeaderVisibility()");
-
-  if (scrollEl?.scrollTop <= 2) {
-    setIsHeaderTop(true);
-    setIsHeaderPinned(false);
-    setIsHeaderUnpinned(false);
-    showHeader();
-  } else {
-    if (isHeaderPinned || isHeaderUnpinned || !isHeaderTop) return;
-    setIsHeaderTop(false);
-    hideHeader();
+    createToggleTabsPanelButton(containerEl);
   }
 }
 
 /**
- * Creates and adds a new header.site-header element to the DOM with 3 inner .headerContainers
+ * ADD LOGGED-OUT NAV TO HEADER
+ * Moves the logged out nav to the .site-header when the user is logged out
+ *
+ * @param {HTMLDivElement} containerEl The container to which the logged-out nav will be appended.
  */
-const createHeader = () => {
-  console.log("Running createHeader()");
+function addLoggedOutNavToHeader(containerEl) {
+  const loggedOutNav = document.querySelector(".columns:has(> #logo + .nav)");
+  if (loggedOutNav) {
+    loggedOutNav.classList.remove("clearfix");
+    loggedOutNav.classList.add("logged-out-nav");
+    containerEl.appendChild(loggedOutNav);
+  }
+}
 
+/**
+ * ADD TITLE TO HEADER WITH NO BREADCRUMBS
+ * If breadcrumbs don't exist, adds the non-default HTML title to the .site-header__container
+ *
+ * @param {HTMLDivElement} containerEl The container to which the title will be appended.
+ */
+function addTitleToHeaderWithNoBreadcrumbs(containerEl) {
+  // Don't add title when the logged-out nav exists.
+  const loggedOutNav = document.querySelector(".columns:has(> #logo + .nav)");
+  if (loggedOutNav) return;
+
+  const titleEl = document.querySelector("title");
+  const titleText = titleEl.innerText;
+  const defaultTitle = "Launch School - an online school for Software Engineers";
+
+  // Don't add the title if it's the default one.
+  if (titleText === defaultTitle) return;
+
+  const newTitleEl = document.createElement("div");
+  newTitleEl.classList.add("title-text");
+  newTitleEl.innerHTML = titleText;
+  containerEl.append(newTitleEl);
+}
+
+/**
+ * CREATE HEADER
+ * Creates a new .site-header element to inject in the DOM
+ */
+function createHeader() {
+  colorLog.run("Running createHeader()");
   const siteHeaderEl = document.createElement("header");
   siteHeaderEl.className = "site-header";
-
-  // Add three div.headerContainers inside `.site-header`
-  for (let i = 0; i < 3; i += 1) {
-    const containerEl = document.createElement("div");
-    containerEl.classList.add("site-header__container", `container-${i + 1}`);
-    siteHeaderEl.appendChild(containerEl);
-  }
-
-  // Insert site header as first element in body
-  document.body.insertBefore(siteHeaderEl, document.body.firstChild);
-  const siteHeader = document.querySelector(".site-header");
-  setHeaderElement(siteHeader);
-};
+  createHeaderContainers(siteHeaderEl);
+  return siteHeaderEl;
+}
 
 /**
- * Creates and adds a button.btn--toggle-tabs-panel element in the .site-header to toggle the Tabs Panel when clicked
- * @param {HTMLElement} parentEL The parent element to which the button will be appended.
+ * CREATE HEADER CONTAINERS
+ * Creates and appends three .site-header__container elements to the .site-header
+ *
+ * @param {HTMLElement} headerEl The header element to which the containers will be appended.
  */
-const createToggleTabsPanelButton = (parentEl) => {
+function createHeaderContainers(headerEl) {
+  for (let i = 0; i < 3; i += 1) {
+    const containerEl = document.createElement("div");
+    const containerNum = i + 1;
+    containerEl.classList.add("site-header__container", `container-${containerNum}`);
+
+    addContainerElements(containerEl, containerNum);
+
+    headerEl.appendChild(containerEl);
+  }
+}
+
+/**
+ * CREATE SHOW SIDEBAR BUTTON
+ * Creates and appends a .btn--show-sidebar element in the .site-header__container to open the sidebar when clicked.
+ *
+ * @param {HTMLDivElement} containerEl The container to which the button will be appended.
+ */
+function createShowSidebarButton(containerEl) {
+  const showSidebarButtonEl = document.createElement("button");
+  showSidebarButtonEl.classList.add("site-header__button", "btn--show-sidebar");
+
+  // Add hamburger icon to button
+  for (let i = 0; i < 3; i += 1) {
+    const lineEl = document.createElement("span");
+    lineEl.classList.add("hamburger-line");
+    showSidebarButtonEl.appendChild(lineEl);
+  }
+
+  containerEl.appendChild(showSidebarButtonEl);
+}
+
+/**
+ * CREATE TOGGLE TABS PANEL BUTTON
+ * Creates and appends a .btn--toggle-tabs-panel element in the .site-header to toggle the Tabs Panel when clicked
+ *
+ * @param {HTMLDivElement} containerEl The container to which the button will be appended.
+ */
+function createToggleTabsPanelButton(containerEl) {
+  const tabsPanel = getTabsPanelElement();
   if (!tabsPanel) return;
 
   const existingButton = document.querySelector(".btn--toggle-tabs-panel");
@@ -137,101 +174,34 @@ const createToggleTabsPanelButton = (parentEl) => {
 
   const tabsPanelToggleButtonEl = document.createElement("button");
   tabsPanelToggleButtonEl.classList.add("site-header__button", "btn--toggle-tabs-panel");
-  tabsPanelToggleButtonEl.innerHTML = "&#10022;";
-  parentEl?.appendChild(tabsPanelToggleButtonEl);
 
-  tabsPanelToggleButton = document.querySelector(".btn--toggle-tabs-panel");
-
-  // Toggle Tabs Panel on button click
-  tabsPanelToggleButton?.addEventListener("click", function () {
-    if (tabsPanel.classList.contains("hidden")) {
-      showTabsPanel();
-    } else {
-      hideTabsPanel();
-    }
-  });
-};
+  containerEl.appendChild(tabsPanelToggleButtonEl);
+}
 
 /**
- * Moves the logged out nav to the .site-header when user is logged out
- * @param {HTMLElement} parentEL The parent element to which the logged-out nav will be appended.
+ * INJECT CONTAINER STYLE OFFSETS
+ * Adds the offset style variables for the .site-header__container elements
  */
-const moveLoggedOutNavToHeader = (parentEl) => {
-  const loggedOutNav = document.querySelector(".columns:has(> #logo + .nav)");
+function injectContainerStyleOffsets() {
+  console.log("Running injectContainerStyleOffsets()");
+  const container1 = document.querySelector(".site-header__container.container-1");
+  const container3 = document.querySelector(".site-header__container.container-3");
 
-  if (!loggedOutNav) return;
+  const container1Width = container1?.offsetWidth || 0;
+  const container3Width = container3?.offsetWidth || 0;
 
-  const hasLoggedOutNavInHeader = header.querySelector(".logged-out-nav");
+  console.log(container1Width, container3Width);
 
-  if (!hasLoggedOutNavInHeader) {
-    loggedOutNav.classList.remove("clearfix");
-    loggedOutNav.classList.add("logged-out-nav");
-    parentEl.appendChild(loggedOutNav);
-  }
-};
-
-// TODO - Update .container to .site-header__container
-/**
- * Adds the offset style variables for the site-header.container elements
- */
-function addHeaderContainerOffsets() {
-  console.log("Running addHeaderContainerOffsets()");
-
-  const headerContainer1Width = headerContainer1.offsetWidth;
-  const headerContainer3Width = headerContainer3.offsetWidth;
   const siteHeaderPadding = 10;
   const gapBetweenContainers = 20;
   const sidebarWidth = 195;
   const navExpandBtnWidth = 30;
-  const headerContainer2LeftOffset = headerContainer1Width + siteHeaderPadding + gapBetweenContainers;
-  const headerContainer2RightOffset = headerContainer3Width + siteHeaderPadding + gapBetweenContainers;
-  const headerContainer2SidebarLeftOffset = sidebarWidth + navExpandBtnWidth + gapBetweenContainers;
+  const container2LeftOffset = container1Width + siteHeaderPadding + gapBetweenContainers;
+  const container2RightOffset = container3Width + siteHeaderPadding + gapBetweenContainers;
+  const container2SidebarLeftOffset = sidebarWidth + navExpandBtnWidth + gapBetweenContainers;
   const root = document.documentElement;
 
-  root.style.setProperty("--header-container2-left-offset", `${headerContainer2LeftOffset}px`);
-  root.style.setProperty("--header-container2-right-offset", `${headerContainer2RightOffset}px`);
-  root.style.setProperty("--header-container2-sidebar-left-offset", `${headerContainer2SidebarLeftOffset}px`);
+  root.style.setProperty("--header-container2-left-offset", `${container2LeftOffset}px`);
+  root.style.setProperty("--header-container2-right-offset", `${container2RightOffset}px`);
+  root.style.setProperty("--header-container2-sidebar-left-offset", `${container2SidebarLeftOffset}px`);
 }
-
-/**
- * Adds the non-default HTML title to the .headerContainer2 element if breadcrumbs don't exist
- * @param {HTMLElement} parentEL The parent element to which the title will be appended.
- */
-const addTitleToHeaderWithNoBreadcrumbs = (parentEl) => {
-  const loggedOutNav = document.querySelector(".columns:has(> #logo + .nav)");
-  if (loggedOutNav) return;
-
-  const titleEl = document.querySelector("title");
-  const title = titleEl.innerText;
-  const defaultTitle = "Launch School - an online school for Software Engineers";
-
-  // Don't add the title if it's the default one.
-  if (title === defaultTitle) return;
-
-  const newTitleEl = document.createElement("div");
-  newTitleEl.classList.add("title-text");
-  newTitleEl.innerHTML = title;
-  parentEl?.append(newTitleEl);
-};
-
-/**
- * Creates and adds a button.btn--show-sidebar element in the .site-header to open the sidebar when clicked.
- * @param {HTMLElement} parentEL The parent element to which the button will be appended.
- */
-const createShowSidebarButton = (parentEl) => {
-  let showSidebarBtn = document.querySelector(".btn--show-sidebar");
-  if (showSidebarBtn) return;
-
-  const createShowSidebarButtonEl = document.createElement("button");
-  createShowSidebarButtonEl.classList.add("site-header__button", "btn--show-sidebar");
-
-  for (let i = 0; i < 3; i += 1) {
-    const lineEl = document.createElement("span");
-    lineEl.classList.add("hamburger-line");
-    createShowSidebarButtonEl.appendChild(lineEl);
-  }
-
-  parentEl?.appendChild(createShowSidebarButtonEl);
-  showSidebarBtn = document.querySelector(".btn--show-sidebar");
-  showSidebarBtn?.addEventListener("click", () => showSidebar());
-};
